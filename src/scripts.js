54176b7d82fd55ea5b3610388fab25fe0c1f841c
cashmirB <128428924+cashmirB@users.noreply.github.com>
2023-03-31 00:37:54 +0100

    created a new variable of city

--- src/scripts.js
+++ src/scripts.js
@@ -44,7 +44,8 @@ function displayTemperature(response) {
 }
 
 let apiKey = "77c45f57f94o3fb60fb856c88ed36ata";
+let city = "Manila";
 
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=London&key=${apiKey}&units=metric`;
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
