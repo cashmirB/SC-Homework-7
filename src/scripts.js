ba8aa53413dc7a5c1b4ef548128eeddd41b9c105
cashmirB <128428924+cashmirB@users.noreply.github.com>
2023-03-31 00:14:34 +0100

    started on new js

--- src/scripts.js
+++ src/scripts.js
@@ -1,4 +1,24 @@
+function displayTemperature(response) {
+  let tempElement = document.querySelector("#currentTemp");
+  tempElement.innerHTML = Math.round(response.data.temperature.current);
+
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.condition.description;
+
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.temperature.humidity;
+
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+
+  console.log(response.data);
+}
+
 let apiKey = "77c45f57f94o3fb60fb856c88ed36ata";
-console.log(apiKey);
+
 let apiUrl = `https://api.shecodes.io/weather/v1/current?query=London&key=${apiKey}&units=metric`;
-console.log(apiUrl);
+
+axios.get(apiUrl).then(displayTemperature);
